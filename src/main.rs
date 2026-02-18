c563f10790eadd059ed2199f40163fa23afd4e7d
linjingpy <qw0615381@outlook.com>
2026-02-18 16:32:49 +0800

    更新

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use std::io::stdin;
-
 fn main() {
     println!("Hello");
     println!("Hello, world!");
+    println!("{}",add(4.0));
+}
+
+fn add(num: f64) ->f64 {   
+    num * 3.1415926 * num
 }
