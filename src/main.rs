2727e4c9e90fe0d8dddd04c7ef124ce671883b91
linjingpy <qw0615381@outlook.com>
2026-02-18 17:18:48 +0800

    更新

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,31 @@
-fn main() {
-    println!("Hello");
-    println!("Hello, world!");
-    println!("{}",add(4.0));
+use std::io;
+
+fn workPi(terms: f64) -> f64 {
+    let mut sum: f64 = 0.0;
+    let mut sign: f64 = 1.0;
+    let mut i: f64 = 1.0;
+    for _ in 0..terms as i64 {
+        sum = sum + sign * 1.0 / (2.0 * i - 1.0);
+        sign = -sign;
+        i += 1.0;
+    }
+    sum * 4.0
 }
 
-fn add(num: f64) ->f64 {   
-    num * 3.1415926 * num
+fn main() {
+    let mut terms_string = String::new();
+
+    println!("请输入迭代次数:");
+    io::stdin().read_line(&mut terms_string).expect("错误");
+
+    let terms: f64 = match terms_string.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("输入无效，请输入数字");
+            return;
+        }
+    };
+
+    let result: f64 = workPi(terms);
+    println!("计算出的圆周率为:{}", result);
 }
