9d2b1252a35352d85b5a2b061af4a51a47cdfe68
linjingpy <qw0615381@outlook.com>
2026-02-19 13:08:39 +0800

    更新

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
-use std::io;
+use clap::Parser;
 
-fn workPi(terms: f64) -> f64 {
+#[derive(Debug,Parser)]
+struct CLI {
+    terms: i64,
+}
+
+
+
+fn work_pi(terms :i64) -> f64 {
     let mut sum: f64 = 0.0;
     let mut sign: f64 = 1.0;
     let mut i: f64 = 1.0;
-    for _ in 0..terms as i64 {
+    for _ in 0..terms  {
         sum = sum + sign * 1.0 / (2.0 * i - 1.0);
         sign = -sign;
         i += 1.0;
@@ -15,21 +22,8 @@ fn workPi(terms: f64) -> f64 {
     sum * 4.0
 }
 fn main() {
-    let mut terms_string = String::new();
+    let cli = CLI::parse();
 
-    println!("欢迎使用圆周率计算器");
-    
-    println!("请输入迭代次数:");
-    io::stdin().read_line(&mut terms_string).expect("错误");
-
-    let terms: f64 = match terms_string.trim().parse() {
-        Ok(num) => num,
-        Err(_) => {
-            println!("输入无效，请输入数字");
-            return;
-        }
-    };
-
-    let result: f64 = workPi(terms);
+    let result: f64 = work_pi(cli.terms);
     println!("计算出的圆周率为:{}", result);
 }
