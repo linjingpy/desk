b0ee0b59be93c24dd8faafae6a1c389c24775ca8
linjingpy <qw0615381@outlook.com>
2026-02-19 18:51:08 +0800

    更新

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,26 @@
 use clap::Parser;
 
-#[derive(Debug,Parser)]
+#[derive(Debug, Parser)]
 struct CLI {
     terms: i64,
 }
 
-
-
-fn work_pi(terms :i64) -> f64 {
+fn work_pi(terms: i64) -> f64 {
     let mut sum: f64 = 0.0;
     let mut sign: f64 = 1.0;
     let mut i: f64 = 1.0;
-    for _ in 0..terms  {
+    for _ in 0..terms {
         sum = sum + sign * 1.0 / (2.0 * i - 1.0);
         sign = -sign;
         i += 1.0;
-
-        
-
     }
     sum * 4.0
 }
+
 fn main() {
     let cli = CLI::parse();
 
     let result: f64 = work_pi(cli.terms);
     println!("计算出的圆周率为:{}", result);
 }
+
